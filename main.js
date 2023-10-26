4ce9d85cdf483f49d4f1a07668e590be95c9a17d
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-10-26 16:58:44 +0200

    aggiunge prima parte js

---
+++ main.js
@@ -0,0 +1,11 @@
+const km = parseInt(prompt("inserisci i km che desideri percorrere"))
+const eta = parseInt(prompt("inserisci la tua età"))
+
+const x = parseInt(18)
+const y = parseInt(65)
+const z = parseInt(0.21)
+
+
+if ( eta < x) {
+    document.getElementsByClassName("message").innerHTML = "sei minorenne"
+} 
