43cf5881c06037a1ef7205665e0a770977ff274c
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-10-27 10:37:41 +0200

    modifica

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 let km = parseInt(prompt("inserisci i km che desideri percorrere"))
 let eta = parseInt(prompt("inserisci la tua età"))
 
-const x = parseInt(18)
-const y = parseInt(65)
+
 const z = parseFloat(0.21)
 
 const prezzo = parseFloat(km * z)
 
-if ( eta < x) {
+if ( eta < 18) {
     prezzoScontato = parseFloat(prezzo -(prezzo * 0.2)).toFixed(2)
     document.getElementById("message").innerHTML= prezzoScontato
-} else if(eta > 65){
+} else if(eta >= 65){
     prezzoScontato = parseFloat(prezzo -(prezzo * 0.4)).toFixed(2)
     document.getElementById("message").innerHTML= prezzoScontato
 } else{
